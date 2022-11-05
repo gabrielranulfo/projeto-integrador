001f767416e8aa7b290378332d06fb2555fd5da8
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2022-11-05 01:38:38 -0300

    Novos testes criados

--- app/tests.py
+++ app/tests.py
@@ -19,5 +19,22 @@ class SimpleTest(unittest.TestCase):
     def test_cadastro_setores_get(self):
         response = self.client.get('/cadastro_setores/')
         self.failUnlessEqual(response.status_code, 200)
-        
+
+    def test_cadastrar_setor_get(self):
+        response = self.client.get('/cadastro_setores/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_create_setor_get(self):
+        response = self.client.get('/cadastro_setores/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_cadastro_equipes_get(self):
+        response = self.client.get('/cadastro_setores/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_cadastrar_equipes_get(self):
+        response = self.client.get('/cadastro_setores/')
+        self.failUnlessEqual(response.status_code, 200)
+
+
 #https://django-portuguese.readthedocs.io/en/1.0/topics/testing.html#escrevendo-testes
